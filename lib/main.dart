9a37ca96a5082ba4a02e73708324db86721facef
sabila pramesti <baesabilsayang@gmail.com>
2022-03-30 10:04:23 +0700

    Layout Profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:profile/profile';
+import 'package:profile/sabila';
 
 void main() => runApp(const MyApp());
 
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'TugasProfil',
-      home: ProfileD(),
+      home: SabilaD(),
     );
   }
 }
